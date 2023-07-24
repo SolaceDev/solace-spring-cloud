955339efc119a2662ee04769d8caef2f0858e08c
Jeffrey Douangpaseuth <11084623+Nephery@users.noreply.github.com>
2023-07-24 14:05:31 -0400

    rm component from healthinvokingsessioneventhandler

--- solace-spring-cloud-stream-binder/solace-spring-cloud-stream-binder-core/src/main/java/com/solace/spring/cloud/stream/binder/health/HealthInvokingSessionEventHandler.java
+++ solace-spring-cloud-stream-binder/solace-spring-cloud-stream-binder-core/src/main/java/com/solace/spring/cloud/stream/binder/health/HealthInvokingSessionEventHandler.java
@@ -4,9 +4,7 @@ import com.solacesystems.jcsmp.SessionEventArgs;
 import com.solacesystems.jcsmp.SessionEventHandler;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class HealthInvokingSessionEventHandler implements SessionEventHandler {
 
     private final SolaceSessionHealthIndicator solaceSessionHealthIndicator;
