594a905d6271949bca19e947e424076fe1b9449a
Heiko Henning <heiko.henning@sbb.ch>
2025-04-24 11:54:48 +0200

    gix nullpointer if micrometer is not ready

--- src/main/java/com/solace/spring/cloud/stream/binder/meter/SolaceMessageMeterBinder.java
+++ src/main/java/com/solace/spring/cloud/stream/binder/meter/SolaceMessageMeterBinder.java
@@ -6,6 +6,9 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.BaseUnits;
 import io.micrometer.core.instrument.binder.MeterBinder;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SolaceMessageMeterBinder implements MeterBinder {
     MeterRegistry registry;
 
@@ -19,12 +22,18 @@ public class SolaceMessageMeterBinder implements MeterBinder {
     public static final String METER_DESCRIPTION_ACTIVE_MESSAGES_SIZE = "Messages active in processing";
     public static final String TAG_NAME = "name";
 
+    public final Map<String, DistributionSummary> meterCache = new ConcurrentHashMap<>();
+
     @Override
     public void bindTo(MeterRegistry registry) {
         this.registry = registry;
     }
 
     public void recordMessage(String bindingName, XMLMessage message) {
+        if (registry == null) {
+            return;
+        }
+
         long payloadSize = message.getAttachmentContentLength() + message.getContentLength();
         registerSizeMeter(METER_NAME_TOTAL_SIZE, METER_DESCRIPTION_TOTAL_SIZE, bindingName)
                 .record(payloadSize + message.getBinaryMetadataContentLength(0));
@@ -33,30 +42,47 @@ public class SolaceMessageMeterBinder implements MeterBinder {
     }
 
     public void recordQueueSize(String bindingName, int queueSize) {
-        DistributionSummary.builder(METER_NAME_QUEUE_SIZE)
-                .description(METER_DESCRIPTION_QUEUE_SIZE)
-                .tag(TAG_NAME, bindingName)
-                .baseUnit(BaseUnits.MESSAGES)
-                .register(registry)
+        if (registry == null) {
+            return;
+        }
+
+        meterCache.computeIfAbsent(
+                        METER_NAME_QUEUE_SIZE + bindingName,
+                        ignored -> DistributionSummary.builder(METER_NAME_QUEUE_SIZE)
+                                .description(METER_DESCRIPTION_QUEUE_SIZE)
+                                .tag(TAG_NAME, bindingName)
+                                .baseUnit(BaseUnits.MESSAGES)
+                                .register(registry)
+                )
                 .record(queueSize);
     }
 
     public void recordActiveMessages(String bindingName, int activeMessages) {
-        DistributionSummary.builder(METER_NAME_ACTIVE_MESSAGES_SIZE)
-                .description(METER_DESCRIPTION_ACTIVE_MESSAGES_SIZE)
-                .tag(TAG_NAME, bindingName)
-                .baseUnit(BaseUnits.MESSAGES)
-                .register(registry)
+        if (registry == null) {
+            return;
+        }
+
+        meterCache.computeIfAbsent(
+                        METER_NAME_ACTIVE_MESSAGES_SIZE + bindingName,
+                        ignored -> DistributionSummary.builder(METER_NAME_ACTIVE_MESSAGES_SIZE)
+                                .description(METER_DESCRIPTION_ACTIVE_MESSAGES_SIZE)
+                                .tag(TAG_NAME, bindingName)
+                                .baseUnit(BaseUnits.MESSAGES)
+                                .register(registry)
+                )
                 .record(activeMessages);
     }
 
     private DistributionSummary registerSizeMeter(String meterName,
                                                   String description,
                                                   String bindingName) {
-        return DistributionSummary.builder(meterName)
-                .description(description)
-                .tag(TAG_NAME, bindingName)
-                .baseUnit(BaseUnits.BYTES)
-                .register(registry);
+        return meterCache.computeIfAbsent(
+                meterName + bindingName,
+                m -> DistributionSummary.builder(meterName)
+                        .description(description)
+                        .tag(TAG_NAME, bindingName)
+                        .baseUnit(BaseUnits.BYTES)
+                        .register(registry)
+        );
     }
 }
